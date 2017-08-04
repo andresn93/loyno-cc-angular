b4edfce1741a6a87e36baccbaf38f35a8cfc71dc
andresn93 <andresn93@gmail.com>
2017-08-03 23:00:11 -0500

    stories html, service, and controller updated

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,44 @@
-angular.module('MainCtrl', []).controller('MainController', function($scope) {
+angular.module('MainCtrl', []).controller('MainController', function($scope, $http, Stories) {
 
-	$scope.tagline = 'To the moon and back!';	
+	// $scope.tagline = 'To the moon and back!';	
+
+//GET ALL PROFILES 
+			Stories.getAllProfiles().then(function(response){
+				console.log(response.data);
+				$scope.stories = response.data;
+			});
+//POST NEW PROFILE
+			$scope.saveProfileFunc = function(){
+				console.log($scope.stories.picture);
+				console.log($scope.stories.name);
+				console.log($scope.stories.description);
+
+					Stories.postNewProfile($scope.stories).then(function(response){
+						console.log($scope.stories);
+						$scope.stories = {};
+
+							Stories.getAllProfiles().then(function(response){
+							console.log(response.data);
+							$scope.stories = response.data;
+						});
+					});
+			},
+//DELETE PROFILE
+			$scope.deleteProfileFunc = function(obj){
+				console.log('ouch delete!');
+				console.log(obj);
+				var delProf = {_id : obj};
+
+					Stories.deleteProfile(delProf).then(function(res, err){
+						if (err){console.log('delete error');}
+						else {console.log(res);}
+
+							Stories.getAllProfiles().then(function(response){
+								console.log(response.data);
+								$scope.stories = response.data;
+							});
+					});
+			}
+	
 
 });
