6dfa4e2eee608302c804f07674217d4b2f663ddd
andresn93 <andresn93@gmail.com>
2017-08-09 12:09:08 -0500

    forms service and ctrl updated

--- public/js/controllers/FormsCtrl.js
+++ public/js/controllers/FormsCtrl.js
@@ -2,8 +2,26 @@ angular.module('FormsCtrl', []).controller('FormsController', function($scope, $
 
 	//FORMS CONTROLLER
 
+		//GET ALL FORMS 
+		Forms.getAllForms().then(function(response){
+			console.log(response.data);
+			$scope.forms = response.data;
+		});
 
+		//DELETE A FORM
+		$scope.deleteFormFunc = function(obj){
+			console.log('ouch delete form');
+			console.log(obj);
+		
+			Forms.deleteForm(delForm).then(function(err, res){
+				if (err){console.log('delete form ERROR');}
+				else {console.log(res);}
 
-
-
+				Forms.getAllForms().then(function(response){
+				console.log(response.data);
+				$scope.forms = response.data;
+			});
+		});
+	}
+	
 });
--- public/js/services/FormsService.js
+++ public/js/services/FormsService.js
@@ -7,20 +7,15 @@ var URL = 'https://gentle-shore-39524.herokuapp.com';
 		getAllForms : function(data){
 			return $http.get(URL + '/', data)
 		},
-//POST A PROFILE
-		postNewForm : function(data){
-			console.log(data);
-			return $http.post(URL + '/', data)
-		},
-//DELETE A PROFILE
+//DELETE A FORM
 		deleteForm  : function(delForm){
 			// console.log(delForm);
 			return $http.post(URL + '/', delForm)
 		}, 
-//EDIT A PROFILE
-		editForm    : function(tempObForm){
-			return $http.post(URL + '/', tempObForm)
-		}
+//EDIT A FORM
+		// editForm    : function(tempObForm){
+		// 	return $http.post(URL + '/', tempObForm)
+		// }
 
 		   }
 
