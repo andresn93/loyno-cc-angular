30c5b22f95ea8779a3e61e92aca4fae59a6c9a33
andresn93 <andresn93@gmail.com>
2017-08-09 12:36:21 -0500

    forms edit and delete working

--- public/js/controllers/FormsCtrl.js
+++ public/js/controllers/FormsCtrl.js
@@ -5,13 +5,14 @@ angular.module('FormsCtrl', []).controller('FormsController', function($scope, $
 		//GET ALL FORMS 
 		Forms.getAllForms().then(function(response){
 			console.log(response.data);
-			$scope.forms = response.data;
+			$scope.applications = response.data;
 		});
 
 		//DELETE A FORM
 		$scope.deleteFormFunc = function(obj){
 			console.log('ouch delete form');
 			console.log(obj);
+			var delForm = {_id : obj}
 		
 			Forms.deleteForm(delForm).then(function(err, res){
 				if (err){console.log('delete form ERROR');}
@@ -19,9 +20,32 @@ angular.module('FormsCtrl', []).controller('FormsController', function($scope, $
 
 				Forms.getAllForms().then(function(response){
 				console.log(response.data);
-				$scope.forms = response.data;
+				$scope.applications = response.data;
 			});
 		});
 	}
-	
+
+		//SELECT FORM: 
+				$scope.selectForm = function(obj){
+					console.log('select form ouch');
+					console.log(obj);
+
+					$scope.selectedForm = obj; 
+				}
+		//EDIT FORM: 
+					$scope.editFormFunc = function(){
+						console.log('poof edit');
+						console.log($scope.selectedForm);
+						var tempObj = $scope.selectedForm;
+
+						Forms.editForm($scope.selectedForm).then(function(res, err){
+							if (err){console.log('edit form error');}
+							else {console.log(res);}
+
+							Forms.getAllForms().then(function(response){
+								$scope.applications = response.data;
+							});
+						});
+					}
+
 });
--- public/js/services/FormsService.js
+++ public/js/services/FormsService.js
@@ -5,17 +5,17 @@ var URL = 'https://gentle-shore-39524.herokuapp.com';
 	return {
 //GET PROFILES
 		getAllForms : function(data){
-			return $http.get(URL + '/', data)
+			return $http.get(URL + '/applications', data)
 		},
 //DELETE A FORM
 		deleteForm  : function(delForm){
 			// console.log(delForm);
-			return $http.post(URL + '/', delForm)
+			return $http.post(URL + '/delete-application', delForm)
 		}, 
 //EDIT A FORM
-		// editForm    : function(tempObForm){
-		// 	return $http.post(URL + '/', tempObForm)
-		// }
+		editForm    : function(tempObForm){
+			return $http.post(URL + '/edit-application', tempObForm)
+		}
 
 		   }
 
